f11c2ae4bd732e29e3a21cb3f7c2d512b23208be
Rahul Verma <raj369verma@gmail.com>
2022-02-04 22:41:23 +0530

    dotenv

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose");
+require ('dotenv'). config ();
 // const mysql = require("mysql")
 
 // const db = mysql.createConnection({
